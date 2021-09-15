b5ff49bb082b85f1f488e90e634be667f6516124
Samuel Mensah <sammens008@gmail.com>
2021-09-15 11:54:29 +0100

    Update variable names

--- evals.py
+++ evals.py
@@ -6,7 +6,7 @@ def evaluate_ts(gold_ts, pred_ts, length):
     
     assert len(gold_ts) == len(pred_ts)
     n_samples = len(gold_ts)
-    n_tp_ts, n_gold_ts, n_pred_ts = 0., 0., 0.
+    n_ts_hits, n_gold_ts, n_pred_ts = 0., 0., 0.
     golden, pred = [], []
 
     for i in range(n_samples):
@@ -21,14 +21,14 @@ def evaluate_ts(gold_ts, pred_ts, length):
         
         assert len(g_ts_sequence) == g_ts.count(1)
 
-        n_tp_ts   += hit_ts_count
+        n_ts_hits   += hit_ts_count
         n_gold_ts += gold_ts_count
         n_pred_ts += pred_ts_count
 
         golden.append(g_ts_sequence)
         pred.append(p_ts_sequence)
 
-    return n_tp_ts, n_gold_ts, n_pred_ts, golden, pred 
+    return n_ts_hits, n_gold_ts, n_pred_ts, golden, pred 
 
 # this function takes the predicted sequence and output the corresponding list of two-tuples.
 # such as the input is ['B', 'I', 'B', 'O', 'O', 'B'].
@@ -40,34 +40,34 @@ def tag2ts(ts_tag_sequence, ts_tag_vocab):
 
     n_tags = len(ts_tag_sequence)
     ts_sequence = []
-    beg, end = -1, -1
+    begin, end = -1, -1
     for i in range(n_tags):
         ts_tag = ts_tag_sequence[i]
         ts_tag = ts_tag_vocab_[ts_tag]
         
         if ts_tag == 'B':
-            if beg != -1:
+            if begin != -1:
                 end = i -1
-                if end >= beg > -1:
-                    ts_sequence.append((beg, end))
-                    beg, end = -1, -1
+                if end >= begin > -1:
+                    ts_sequence.append((begin, end))
+                    begin, end = -1, -1
                 else:
-                    beg, end = -1, -1
+                    begin, end = -1, -1
 
-            beg = i
+            begin = i
             end = i
         elif ts_tag == 'O':
             end = i-1
-            if end >= beg > -1:
-                ts_sequence.append((beg, end))
-                beg, end = -1, -1
+            if end >= begin > -1:
+                ts_sequence.append((begin, end))
+                begin, end = -1, -1
             else:
-                beg, end = -1, -1
+                begin, end = -1, -1
         elif ts_tag == 'I':
             end = i
 
-    if end >= beg > -1:
-        ts_sequence.append((beg, end))
+    if end >= begin > -1:
+        ts_sequence.append((begin, end))
 
     return ts_sequence
 
@@ -81,11 +81,11 @@ def match_ts(gold_ts_sequence, pred_ts_sequence):
             hit_count += 1
         pred_count += 1
 
-    return hit_count, gold_count, pred_count # number of right, number of ground truth, number of extracted
+    return hit_count, gold_count, pred_count # number of hits, number of ground truth, number of extracted
 
 def evaluate_program(trainer, batches, args):
     eval_opn_loss, eval_step  = 0., 0
-    labels_opn_n, logits_opn_n, rights_opn_n = 0., 0., 0.
+    labels_opn_n, logits_opn_n, hits_opn_n = 0., 0., 0.
     golden_opn, pred_opn = [], []
     for batch in batches:
         loss, pred = trainer.predict(batch)
@@ -93,21 +93,21 @@ def evaluate_program(trainer, batches, args):
         eval_step += 1
 
         length = batch[7].sum(dim=1).tolist()
-        rights_n_t, labels_n_t, logits_n_t, _, _ = evaluate_ts(batch[-1].tolist(), pred, length) # rights_n_t: number of right, number of ground truth, number of extracted in this batch
+        hits_n_t, labels_n_t, logits_n_t, _, _ = evaluate_ts(batch[-1].tolist(), pred, length) # hits_n_t: number of hits, number of ground truth, number of extracted in this batch
         labels_opn_n += labels_n_t
         logits_opn_n += logits_n_t
-        rights_opn_n += rights_n_t
+        hits_opn_n += hits_n_t
 
     # opn F1
-    prec   = rights_opn_n / (logits_opn_n + 1e-10)
-    recall = rights_opn_n / (labels_opn_n + 1e-10)
+    prec   = hits_opn_n / (logits_opn_n + 1e-10)
+    recall = hits_opn_n / (labels_opn_n + 1e-10)
     F1_opn = 2 * prec * recall / (prec + recall + 1e-10)
 
     return [prec, recall, F1_opn], eval_opn_loss 
 
 def evaluate_program_case_study(trainer, batches, args):
     eval_opn_loss, eval_step  = 0., 0
-    labels_opn_n, logits_opn_n, rights_opn_n = 0., 0., 0.
+    labels_opn_n, logits_opn_n, hits_opn_n = 0., 0., 0.
     golden_opn, pred_opn = [], []
     sample_id, golden, pred_span = [], [], []
     for batch in batches:
@@ -116,18 +116,18 @@ def evaluate_program_case_study(trainer, batches, args):
         eval_step += 1
 
         length = batch[7].sum(dim=1).tolist()
-        rights_n_t, labels_n_t, logits_n_t, golden_t, pred_t = evaluate_ts(batch[-1].tolist(), pred, length)
+        hits_n_t, labels_n_t, logits_n_t, golden_t, pred_t = evaluate_ts(batch[-1].tolist(), pred, length)
         labels_opn_n += labels_n_t
         logits_opn_n += logits_n_t
-        rights_opn_n += rights_n_t
+        hits_opn_n += hits_n_t
 
         sample_id.extend(batch[0])
         golden.extend(golden_t)
         pred_span.extend(pred_t)
 
     # opn F1
-    prec   = rights_opn_n / (logits_opn_n + 1e-10)
-    recall = rights_opn_n / (labels_opn_n + 1e-10)
+    prec   = hits_opn_n / (logits_opn_n + 1e-10)
+    recall = hits_opn_n / (labels_opn_n + 1e-10)
     F1_opn = 2 * prec * recall / (prec + recall + 1e-10)
 
     return [prec, recall, F1_opn], [sample_id, golden, pred_span]
